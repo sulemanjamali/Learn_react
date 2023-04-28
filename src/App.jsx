bb2c04b26f437b35f366478c6fb7250c670d9c18
Suleman Jamali <sulemanjamali98@gmail.com>
2023-04-28 18:26:21 +0500

    all done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Kashan,{Suleman,Dev} from "./Components/Friend1"
+
+import React from 'react'
+
+export default function App() {
+  return (
+    
+    <>
+    <Dev/>
+    <Kashan/>
+    <Suleman/>
+    </>
+  )
+}
+
---
+++ src/Components/Friend1.jsx
@@ -0,0 +1,28 @@
+export function Suleman() {
+  
+    return (
+      <>
+        <p>Hey Suleman</p>
+          
+      </>
+    )
+  }
+  
+  
+  export function Dev() {
+    return (
+      <>
+      <p>Hey Dev</p>
+      </>
+    )
+  }
+  
+  function Izaan() {
+    return (
+      <>
+      <p>Hey Izaan</p>
+      </>
+    )
+  }
+  
+  export default Izaan;
